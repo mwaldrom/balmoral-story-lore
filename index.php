39cb12c2c0bd7f91531781034569aff162777403
Michael Waldrom <mwaldrom@gmail.com>
2022-03-18 15:25:07 +1300

    add two columns

--- index.php
+++ index.php
@@ -16,13 +16,16 @@ $page_blerb = "These are the mechanic's used in writing the story, not generally
     <?php include 'header.php';?>
 </header>
 <main class="container">
-    
-    <h1>The world building and background info need for the write of the series and help with discrepancy of all parties involves</h1>
-    
-    <figure>
-        <figcaption class="blerb">Balmoral Family Adventure's, is a series of <strong>science fantasy flash fiction young adult</strong> stories.<br>From an <strong>all-seeing all-knowing</strong> narrator, with <strong>time warping and bending of real-world</strong> facts a given.</figcaption>
-        <img class="responsive-img" src="/img/crest-large.jpg">
-    </figure>
+<div class="row">
+      <div class="col s12 l6">
+            <figure>
+                <figcaption class="blerb">Balmoral Family Adventure's, is a series of <strong>science fantasy flash fiction young adult</strong> stories.<br>From an <strong>all-seeing all-knowing</strong> narrator, with <strong>time warping and bending of real-world</strong> facts a given.</figcaption>
+                <img class="responsive-img" src="/img/crest-large.jpg">
+            </figure>
+        </div>
+
+      <div class="col s12 l6 blerb"><p>The world building and background info need for the write of the series and help with discrepancy of all parties involves</p>
+      <p>This is a work of fiction. Names, characters, businesses, places, events and incidents are either the products of the author’s imagination or used in a fictitious manner. Any resemblance to actual persons, living or dead, or actual events is purely coincidental.</p></div></div>
         
 </main>
 <footer>
